07948a2d397fe11a9b62dc5960a5ac7f6a858b61
воробйов александр <avorobeov21@gmail.com>
2023-04-13 20:20:46 +0300

    Добавьте файлы проекта.

---
+++ Homework number 1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_number_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isDead;
+            int numberLives;
+            uint damage;
+            short money;
+            long level;
+            ulong maximumDamage;
+            string name;
+            float age;
+            decimal weight;
+            double armor;
+        }
+    }
+}
